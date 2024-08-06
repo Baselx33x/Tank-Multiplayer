668e4053a10f54c7ecc24bea117ae9fc879f54b2
BaselX <Basel.x33x@gmail.com>
2024-08-06 14:50:29 +0200

    Added UI Manager Script
    1. Bind the Button for the Server , Host and Client.

---
+++ Assets/Scripts/UI Manager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] Button m_ServerButton;
+    [SerializeField] Button m_HostButton;
+    [SerializeField] Button m_ClientButton;
+
+
+    private void Awake()
+    {
+        m_ServerButton.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
+        m_HostButton.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
+        m_ClientButton.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+    }
+
+}
